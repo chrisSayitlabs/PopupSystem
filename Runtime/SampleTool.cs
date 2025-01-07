34c9f3452dd31f3e14f600c41accfdcf0c0478d9
Chris <chris@sayitlabs.com>
2025-01-07 17:20:57 +0100

    initial commit.

---
+++ Runtime/SampleTool.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SayItLabs.PopupSystem
+{
+    public class SampleTool : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("yes yes!");
+        }
+    }
+}
