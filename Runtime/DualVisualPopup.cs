784b64d014a68ef45a432014f1e61920b0afe92a
Chris <chris@sayitlabs.com>
2025-07-09 15:14:45 +0200

    Adding in implementation of initialization for dual visual popup.

--- Runtime/DualVisualPopup.cs
+++ Runtime/DualVisualPopup.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SayItLabs.PopupSystem
 {
-    public class DualVisualPopup : MonoBehaviour
+    public class DualVisualPopup : BasePopup
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("Images")]
+        [SerializeField] private Image leftSprite;
+        [SerializeField] private Image rightSprite;
+        [Header("Buttons")]
+        [SerializeField] private Button leftButton;
+        [SerializeField] private Button rightButton;
+        public override void InitializePopup(PopupInfo pi)
         {
-        
-        }
+            leftSprite.sprite = pi.LeftSprite;
+            rightSprite.sprite = pi.RightSprite;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+            if (leftButton != null)
+            {
+                TextMeshProUGUI tmp = leftButton.GetComponentInChildren<TextMeshProUGUI>();
+                if (tmp != null)
+                    tmp.text = pi.GetString(EPopupStringType.LeftButtonLabel);
+                leftButton.onClick.AddListener(() => OnPopupConfirmation());
+            }
+
+            if (rightButton != null)
+            {
+                TextMeshProUGUI tmp = rightButton.GetComponentInChildren<TextMeshProUGUI>();
+                if (tmp != null)
+                    tmp.text = pi.GetString(EPopupStringType.RightButtonLabel);
+                rightButton.onClick.AddListener(() => OnPopupRefuse());
+            }
         }
     }
 }
