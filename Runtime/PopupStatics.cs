81331c538de09e7671e7e2777cced93fdce8c932
Chris <chris@sayitlabs.com>
2025-07-09 15:05:09 +0200

    Created statics for indexes of buttons.

---
+++ Runtime/PopupStatics.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SayItLabs.PopupSystem
+{
+    public static class PopupStatics
+    {
+        public static int BUTTON_1 = 1;
+        public static int BUTTON_2 = 2;
+    }
+}
